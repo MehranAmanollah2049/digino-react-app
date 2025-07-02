d41ac6bae0a004746094d5fd8b3c96acf863dece
Mehran <mehranamanollah13@gmail.com>
2025-07-02 12:51:05 +0330

    logs removed

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -42,9 +42,7 @@ export default function Login() {
             setLoading(false)
           }
         })
-        .catch((e) => {
-          console.log(e);
-          
+        .catch(() => {          
           setLoading(false)
         })
     }
