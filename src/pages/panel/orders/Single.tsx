b6bc20c6c9d1cbb179b7b52c07204e47750ff920
Mehran <mehranamanollah13@gmail.com>
2025-07-08 16:59:46 +0330

    responsive bug fixed at panel for single order page

--- src/pages/panel/orders/Single.tsx
+++ src/pages/panel/orders/Single.tsx
@@ -119,88 +119,111 @@ export default function Single() {
 
     return (
         <>
-            <div className="w-full bg-white rounded-xl px-10 py-7 max-[500px]:px-5">
+            <div className="w-full bg-white rounded-xl px-5 py-7 min-[500px]:px-10">
 
-                <div className="w-full flex items-center justify-between mb-8 max-[340px]:flex-col gap-4">
-                    <p className="font-heavy text-[20px] max-[500px]:text-[18px] text-theme pr-5 flex items-center justify-center relative after:size-2 after:rounded-full after:bg-theme after:absolute after:content-[''] after:transform-[translateY(-0.5px)] after:right-0"> جزییات سفارش</p>
+                <div className="w-full flex items-center justify-between mb-8 flex-col gap-4 min-[340px]:flex-row">
+                    <p className="font-heavy text-[18px] min-[500px]:text-[20px] text-theme pr-5 flex items-center justify-center relative after:size-2 after:rounded-full after:bg-theme after:absolute after:content-[''] after:transform-[translateY(-0.5px)] after:right-0">
+                        جزییات سفارش
+                    </p>
 
                     {
                         !isLoading ? (
-                            <div className={`h-[28px] px-3 font-medium rounded-full text-[15px] flex items-center justify-center ${data.status == Status.SUCCESS ? 'bg-[#00c07318] text-[#00c073]' : 'bg-[#ed19431a] text-[#ed1943]'}`}>{data.status}</div>
+                            <div
+                                className={`h-[28px] px-3 font-medium rounded-full text-[15px] flex items-center justify-center ${data.status == Status.SUCCESS ? 'bg-[#00c07318] text-[#00c073]' : 'bg-[#ed19431a] text-[#ed1943]'}`}>
+                                {data.status}
+                            </div>
                         ) : <div className="w-[70px] h-[13px] bg-gray-200 rounded-full animation-pluse" v-if="isLoading"></div>
                     }
-
                 </div>
 
                 <div className="w-full flex items-center justify-center flex-wrap gap-y-4 border-b border-gray-200/60 pb-6">
-                    <div className="w-1/4 flex items-start justify-center flex-col max-[800px]:w-2/4 max-[400px]:w-full">
+                    <div className="w-full flex items-start justify-center flex-col min-[400px]:w-2/4 min-[800px]:w-1/4">
                         <p className="text-[15px] font-medium text-gray-400"># کد تراکنش</p>
                         {
-                            !isLoading ? <span className="text-[17px] font-bold text-dark-200">{data.code}</span> : <span className="w-[60px] h-[13px] bg-gray-200 animation-pluse rounded-full mt-1"></span>
+                            !isLoading
+                                ? <span className="text-[17px] font-bold text-dark-200">{data.code}</span>
+                                : <span className="w-[60px] h-[13px] bg-gray-200 animation-pluse rounded-full mt-1"></span>
                         }
                     </div>
-                    <div className="w-1/4 flex items-start justify-center flex-col max-[800px]:w-2/4 max-[400px]:w-full">
+                    <div className="w-full flex items-start justify-center flex-col min-[400px]:w-2/4 min-[800px]:w-1/4">
                         <p className="text-[15px] font-medium text-gray-400">تاریخ</p>
                         {
-                            !isLoading ? <span className="text-[17px] font-bold text-dark-200">{data.created_at}</span> : <span className="w-[60px] h-[13px] bg-gray-200 animation-pluse rounded-full mt-1"></span>
+                            !isLoading
+                                ? <span className="text-[17px] font-bold text-dark-200">{data.created_at}</span>
+                                : <span className="w-[60px] h-[13px] bg-gray-200 animation-pluse rounded-full mt-1"></span>
                         }
                     </div>
-                    <div className="w-1/4 flex items-start justify-center flex-col max-[800px]:w-2/4 max-[400px]:w-full">
+                    <div className="w-full flex items-start justify-center flex-col min-[400px]:w-2/4 min-[800px]:w-1/4">
                         <p className="text-[15px] font-medium text-gray-400">شماره موبایل</p>
                         {
-                            !isLoading ? <span className="text-[17px] font-bold text-dark-200">{data.phone}</span> : <span className="w-[60px] h-[13px] bg-gray-200 animation-pluse rounded-full mt-1"></span>
+                            !isLoading
+                                ? <span className="text-[17px] font-bold text-dark-200">{data.phone}</span>
+                                : <span className="w-[60px] h-[13px] bg-gray-200 animation-pluse rounded-full mt-1"></span>
                         }
                     </div>
-                    <div className="w-1/4 flex items-start justify-center flex-col max-[800px]:w-2/4 max-[400px]:w-full">
+                    <div className="w-full flex items-start justify-center flex-col min-[400px]:w-2/4 min-[800px]:w-1/4">
                         <p className="text-[15px] font-medium text-gray-400">تحویل گیرنده</p>
                         {
-                            !isLoading ? <span className="text-[17px] font-bold text-dark-200">{data.full_name}</span> : <span className="w-[60px] h-[13px] bg-gray-200 animation-pluse rounded-full mt-1"></span>
+                            !isLoading
+                                ? <span className="text-[17px] font-bold text-dark-200">{data.full_name}</span>
+                                : <span className="w-[60px] h-[13px] bg-gray-200 animation-pluse rounded-full mt-1"></span>
                         }
                     </div>
                 </div>
+
                 <div className="w-full flex items-center justify-center flex-wrap gap-y-4 pt-6 pb-3">
-                    <div className="w-1/4 flex items-start justify-center flex-col max-[800px]:w-2/4 max-[400px]:w-full">
+                    <div className="w-full flex items-start justify-center flex-col min-[400px]:w-2/4 min-[800px]:w-1/4">
                         <p className="text-[15px] font-medium text-gray-400">جمع کل</p>
                         {
-                            !isLoading ? <span className="text-[17px] font-bold text-dark-200">{total_price.toLocaleString()} تومان</span> : <span className="w-[60px] h-[13px] bg-gray-200 animation-pluse rounded-full mt-1"></span>
+                            !isLoading
+                                ? <span className="text-[17px] font-bold text-dark-200">{total_price.toLocaleString()} تومان</span>
+                                : <span className="w-[60px] h-[13px] bg-gray-200 animation-pluse rounded-full mt-1"></span>
                         }
                     </div>
-                    <div className="w-1/4 flex items-start justify-center flex-col max-[800px]:w-2/4 max-[400px]:w-full">
+                    <div className="w-full flex items-start justify-center flex-col min-[400px]:w-2/4 min-[800px]:w-1/4">
                         <p className="text-[15px] font-medium text-gray-400">سود شما</p>
                         {
-                            !isLoading ? <span className="text-[17px] font-bold text-dark-200">{total_discount.toLocaleString()} تومان</span> : <span className="w-[60px] h-[13px] bg-gray-200 animation-pluse rounded-full mt-1"></span>
+                            !isLoading
+                                ? <span className="text-[17px] font-bold text-dark-200">{total_discount.toLocaleString()} تومان</span>
+                                : <span className="w-[60px] h-[13px] bg-gray-200 animation-pluse rounded-full mt-1"></span>
                         }
                     </div>
-                    <div className="w-1/4 flex items-start justify-center flex-col max-[800px]:w-2/4 max-[400px]:w-full">
-                        <p className="text-[15px] font-medium text-gray-400">{data.status == Status.FAIL ? 'قابل پرداخت' : 'پرداخت شده '} </p>
+                    <div className="w-full flex items-start justify-center flex-col min-[400px]:w-2/4 min-[800px]:w-1/4">
+                        <p className="text-[15px] font-medium text-gray-400">{data.status == Status.FAIL ? 'قابل پرداخت' : 'پرداخت شده '}</p>
                         {
-                            !isLoading ? <span className="text-[17px] font-bold text-dark-200">{total_payment.toLocaleString()} تومان</span> : <span className="w-[60px] h-[13px] bg-gray-200 animation-pluse rounded-full mt-1"></span>
+                            !isLoading
+                                ? <span className="text-[17px] font-bold text-dark-200">{total_payment.toLocaleString()} تومان</span>
+                                : <span className="w-[60px] h-[13px] bg-gray-200 animation-pluse rounded-full mt-1"></span>
                         }
                     </div>
-                    <div className="w-1/4 flex items-start justify-center flex-col max-[800px]:w-2/4 max-[400px]:w-full">
+                    <div className="w-full flex items-start justify-center flex-col min-[400px]:w-2/4 min-[800px]:w-1/4">
                         <p className="text-[15px] font-medium text-gray-400">درگاه پرداخت</p>
                         {
-                            !isLoading ? <span className="text-[17px] font-bold text-dark-200">{data.driver}</span> : <span className="w-[60px] h-[13px] bg-gray-200 animation-pluse rounded-full mt-1"></span>
+                            !isLoading
+                                ? <span className="text-[17px] font-bold text-dark-200">{data.driver}</span>
+                                : <span className="w-[60px] h-[13px] bg-gray-200 animation-pluse rounded-full mt-1"></span>
                         }
                     </div>
                 </div>
             </div>
-            <div className="w-full bg-white rounded-xl px-10 py-7 mt-7 max-[500px]:px-5">
-                <div className="w-full flex items-center justify-between mb-8 max-[450px]:flex-col max-[450px]:gap-4">
-                    <p
-                        className="font-heavy text-[20px] max-[500px]:text-[18px] text-theme pr-5 flex items-center justify-center relative after:size-2 after:rounded-full after:bg-theme after:absolute after:content-[''] after:transform-[translateY(-0.5px)] after:right-0">
-                        کالا های سفارشی</p>
+
+            <div className="w-full bg-white rounded-xl px-5 py-7 mt-7 min-[500px]:px-10">
+                <div className="w-full flex items-center justify-between mb-8 flex-col gap-4 min-[450px]:flex-row">
+                    <p className="font-heavy text-[18px] min-[500px]:text-[20px] text-theme pr-5 flex items-center justify-center relative after:size-2 after:rounded-full after:bg-theme after:absolute after:content-[''] after:transform-[translateY(-0.5px)] after:right-0">
+                        کالا های سفارشی
+                    </p>
 
                     {
                         data.status == Status.FAIL && !isLoading ? (
-                            <div onClick={payment_handler} className={`btn-title !w-auto transition-all w-full h-[45px] rounded-lg text-[15px] font-bold max-[450px]:!w-full ${isPaymentLoading && 'loading'}`}>
+                            <div
+                                onClick={payment_handler}
+                                className={`btn-title !w-full transition-all h-[45px] rounded-lg text-[15px] font-bold min-[450px]:!w-auto ${isPaymentLoading && 'loading'}`}>
                                 {
                                     isPaymentLoading ? <Loading /> : 'نهایی سازی سفارش'
                                 }
                             </div>
                         ) : ''
                     }
-
                 </div>
 
                 <div className="w-full flex items-center justify-center flex-col gap-3">
@@ -219,6 +242,7 @@ export default function Single() {
                     }
                 </div>
             </div>
+
         </>
     )
 }
