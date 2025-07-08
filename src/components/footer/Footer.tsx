08ddcddc504bb046e4cf910d4bba70e609618b3d
Mehran <mehranamanollah13@gmail.com>
2025-07-08 19:45:13 +0330

    version updated in footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -3,7 +3,7 @@ export default function Footer() {
     return (
         <footer className="w-full flex items-center justify-center py-10 mb-[60px] min-[800px]:mb-[0px]">
             <p className="text-gray-500 font-medium">
-                <span className="text-theme">Digino</span> - v.0.1
+                <span className="text-theme">Digino</span> - v.1.2
             </p>
         </footer>
     )
