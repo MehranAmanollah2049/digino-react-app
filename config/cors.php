f0816ebc3a2caaa23272249106ad84d674f931ba
Mehran <mehranamanollah13@gmail.com>
2025-07-09 18:23:58 +0330

    cross-origin added

---
+++ config/cors.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+
+    'allowed_methods' => ['*'], // همه متدها (GET, POST, PUT, DELETE)
+
+    'allowed_origins' => ['https://store.mehran-dev.com'],
+
+    'allowed_origins_patterns' => [],
+
+    'allowed_headers' => ['*'], // همه هدرها
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => true, // اگر کوکی و سشن نیاز دارید
+];
